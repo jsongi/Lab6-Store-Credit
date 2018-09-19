71c7e0e6b55c3244614acbb6af28f8041c800152
Wang, Alan <1843324@00-63126.local>
2018-09-19 08:39:33 -0700

    initial commit + psuedocode

--- Lab6-Store Credit/main.c
+++ Lab6-Store Credit/main.c
@@ -4,12 +4,42 @@
 //
 //  Created by Wang, Alan on 9/19/18.
 //  Copyright © 2018 Alan Wang. All rights reserved.
+//  Phase 1:
+//  Account number
+//  Customer name
+//  Beginning balance
+//  Total charges
+//  Total payments
+//  Credit limit
+//  New balance
+//  Credit remaining
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
+#define CREDIT_LIMIT 500
+int main()
+{
+    // Phase 2:
+    int account_num;
+    char customer_name[40];
+    float current_balance;
+    float total_charges;
+    float total_payments;
+    float new_balance;
+    float credit_left;
+    
+    // Phase 3:
+    // I: Input data
+    // A: Prompt user for data
+    // B: Use scanf to retrieve data
+    
+    // II: Calculate new balance
+    // A: add balance and charges
+    // B: Subtract payments
+    // C: subtract from credit limit
+    
+    // III: Output data
+    // A: Print data using printf
     printf("Hello, World!\n");
     return 0;
 }
