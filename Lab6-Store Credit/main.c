4d77f7078d97ff323be6ca443b44949b7b3544c7
Wang, Alan <1843324@00-63126.local>
2018-09-19 08:31:58 -0700

    Initial Commit

---
+++ Lab6-Store Credit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab6-Store Credit
+//
+//  Created by Wang, Alan on 9/19/18.
+//  Copyright © 2018 Alan Wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
